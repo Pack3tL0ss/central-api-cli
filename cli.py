8896b84795fb626f2ba11ab5dd3cf9f15e8d218e
Wade Wells ~ Pack3tL0ss <github.com Pack3tL0ss>
2020-08-27 15:03:12 -0500

    initial commit (WIP)

---
+++ cli.py
@@ -0,0 +1,299 @@
+#!/usr/bin/env python3
+
+from enum import Enum
+# from pprint import pprint
+# from typing import Union
+# from pathlib import Path
+# from requests.sessions import session
+
+import typer
+from lib.centralCLI import CentralApi, BuildCLI, utils
+import os
+
+_config_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), "config")
+_def_import_file = os.path.join(_config_dir, "stored-tasks.yaml")
+
+# import click_spinner  # NoQA
+
+
+# -- break up arguments passed as single string from vscode promptString --
+def get_arguments_from_import(import_file: str, key: str = None):
+    args = utils.read_yaml(_import_file)
+    if key and key in args:
+        args = args[key]
+
+    sys.argv += args
+
+    return sys.argv
+
+
+import sys  # NoQA
+sys.argv[0] = 'cencli'
+try:
+    if len(sys.argv) > 1:
+        if " " in sys.argv[1] or not sys.argv[1]:
+            vsc_args = sys.argv.pop(1)
+            if vsc_args:
+                sys.argv += vsc_args.split()
+
+    if len(sys.argv) > 2:
+        _import_file, _import_key = None, None
+        if sys.argv[2].endswith((".yaml", ".yml")):
+            _import_file = sys.argv.pop(2)
+            if not utils.valid_file(_import_file):
+                if utils.valid_file(os.path.join(_config_dir, _import_file)):
+                    _import_file = os.path.join(_config_dir, _import_file)
+
+            if len(sys.argv) > 2:
+                _import_key = sys.argv.pop(2)
+
+        sys.argv = get_arguments_from_import(_import_file, key=_import_key)
+except Exception:
+    pass
+
+
+app = typer.Typer()
+
+
+class ShowLevel2(str, Enum):
+    devices = "devices"
+    # devices = "dev"
+    switch = "switch"
+    groups = "groups"
+    sites = "sites"
+    clients = "clients"
+    aps = "ap"
+    gateway = "gateway"
+    # gateways = "gateways"
+
+
+def caas_response(resp):
+    if not resp.ok:
+        typer.echo(f"[{resp.status_code}] {resp.text} {resp.reason}")
+        return
+    else:
+        resp = resp.json()
+
+    print()
+    lines = "-" * 22
+    typer.echo(lines)
+    if resp.get("_global_result", {}).get("status", '') == 0:
+        typer.echo("Global Result: Success")
+    else:
+        typer.echo("Global Result: Failure")
+    typer.echo(lines)
+    _bypass = None
+    if resp.get("cli_cmds_result"):
+        typer.echo("\n -- Command Results --")
+        for cmd_resp in resp["cli_cmds_result"]:
+            for _c, _r in cmd_resp.items():
+                _r_code = _r.get("status")
+                if _r_code == 0:
+                    _r_pretty = "OK"
+                else:
+                    _r_pretty = f"ERROR {_r_code}"
+                _r_txt = _r.get("status_str")
+                typer.echo(f" [{_bypass or _r_pretty}] {_c}")
+                if not _r_code == 0:
+                    _bypass = "bypassed"
+                    if _r_txt:
+                        typer.echo(f"\t{_r_txt}\n")
+                    typer.echo("-" * 65)
+                    typer.echo("!! Remaining Commands bypassed due to Error in previous object !!")
+                    typer.echo("-" * 65)
+                elif _r_txt and not _bypass:
+                    typer.echo(f"\t{_r_txt}")
+        print()
+
+
+# def _pretty(content: Union[str, list]):
+#     '''A Convenience Function for Colorizing and echoing Text
+
+#     Place text to be colorized in the text str as {{RED:This text will display as red}}
+#     i.e.: "An {{RED:ERROR}} occured"  or "An {{RED:BOLD:ERROR}} occured"
+#     With f strings: f"This is a {{RED:BOLD:{var}}} test."
+
+#     Args:
+#         content (str|list): Text to be colorized (or list of strings)
+#     '''
+#     if content.count("{") != content.count("}"):
+#         typer.echo(f"malformed str passed to _pretty function\n\t{content}")
+#     else:
+#         _this = content
+#         r_list = []
+#         while True:
+#             r_list.append(_this.split("{")[-1].split("}")[0])
+
+    # _msg = typer.style(f"{key} not found in {import_file}.  No Data to Process", fg=typer.colors.RED, bold=True)
+    # typer.echo(_msg)
+
+@app.command()
+def bulk_edit(input_file: str = typer.Argument(None)):
+    session = CentralApi()
+    cli = BuildCLI(session=session)
+    # TODO log cli
+    if cli.cmds:
+        for dev in cli.data:
+            group_dev = f"{cli.data[dev]['_common'].get('group')}/{dev}"
+            resp = session.caasapi(group_dev, cli.cmds)
+            caas_response(resp)
+
+
+@app.command()
+def show(what: ShowLevel2 = typer.Argument(...), dev_type: str = typer.Argument(None), group: str = None):
+    _data = None
+    _spin_txt = "Establishing Session with Aruba Central API Gateway..."
+    session = utils.spinner(_spin_txt, CentralApi)
+    # with click_spinner.spinner():
+    #     session = CentralApi()
+
+    if not dev_type:
+        if what.startswith("gateway"):
+            what, dev_type = "devices", "gateway"
+
+        elif what.lower() in ["iap", "ap", "aps"]:
+            what, dev_type = "devices", "iap"
+
+        elif what.lower() == "switch":
+            what, dev_type = "devices", "switch"
+
+    if what == "devices":
+        if dev_type:
+            dev_type = "gateway" if dev_type == "gateways" else dev_type
+            if not group:
+                _data = session.get_dev_by_type(dev_type)
+            else:
+                _data = session.get_gateways_by_group(group)
+
+    elif what == "groups":
+        _data = session.get_all_groups()
+
+    # print(_data)
+    if _data:
+        typer.echo("\n--")
+        if isinstance(_data, dict):
+            _data = _data.get("data", _data)
+
+        for _ in _data:
+            if isinstance(_, list) and len(_) == 1:
+                typer.echo(_[0])
+
+            elif isinstance(_, dict):
+                typer.echo("--")
+                # _c_id, _c_name = None, None
+                # if not cust_echo and k in ["customer_id", "customer_name"]:
+                for k, v in _.items():
+                    typer.echo(f"{k}: {v}")
+            else:
+                typer.echo(_)
+
+        typer.echo("--\n")
+
+
+@app.command()
+def add_vlan(group_dev: str = typer.Argument(...), pvid: str = typer.Argument(...), ip: str = typer.Argument(None),
+             mask: str = typer.Argument("255.255.255.0"), name: str = None, description: str = None,
+             interface: str = None, vrid: str = None, vrrp_ip: str = None, vrrp_pri: int = None):
+    cmds = []
+    cmds += [f"vlan {pvid}", "!"]
+    if name:
+        cmds += [f"vlan-name {name}", "!", f"vlan {name} {pvid}", "!"]
+    if ip:
+        _fallback_desc = f"VLAN{pvid}-SVI"
+        cmds += [f"interface vlan {pvid}", f"description {description or name or _fallback_desc}", f"ip address {ip} {mask}", "!"]
+    if vrid:
+        cmds += [f"vrrp {vrid}", f"ip address {vrrp_ip}", f"vlan {pvid}"]
+        if vrrp_pri:
+            cmds += [f"priority {vrrp_pri}"]
+        cmds += ["no shutdown", "!"]
+
+    session = CentralApi()
+    # TODO move command gen to BuildCLI
+    caas_response(session.caasapi(group_dev, cmds))
+    # for c in cmds:
+    #     typer.echo(c)
+
+
+@app.command()
+def import_vlan(import_file: str = typer.Argument(_def_import_file),
+                key: str = None):
+    if import_file == _def_import_file and not key:
+        typer.echo("key is required when using the default import file")
+
+    data = utils.read_yaml(import_file)
+    if key:
+        data = data.get(key)
+
+    if data:
+        args = data.get("arguments", [])
+        kwargs = data.get("options", {})
+        add_vlan(*args, **kwargs)
+
+
+@app.command()
+def batch(import_file: str = typer.Argument(_def_import_file),
+          command: str = None, key: str = None):
+
+    if import_file == _def_import_file and not key:
+        typer.echo("key is required when using the default import file")
+        # typer.Exit()
+
+    data = utils.read_yaml(import_file)
+    if key:
+        data = data.get(key)
+
+    if not data:
+        _msg = typer.style(f"{key} not found in {import_file}.  No Data to Process", fg=typer.colors.RED, bold=True)
+        typer.echo(_msg)
+    else:
+        args = data.get("arguments", [])
+        kwargs = data.get("options", {})
+        cmds = data.get("cmds", [])
+
+        if not args:
+            pass  # TODO error msg import data requires an argument specifying the group / device
+
+        if command:
+            try:
+                exec(f"fn = {command}")
+                fn(*args, **kwargs)  # type: ignore # NoQA
+            except AttributeError:
+                typer.echo(f"{command} doesn't appear to be valid")
+        elif cmds:
+            # if "!" not in cmds:
+            #     cmds = '^!^'.join(cmds).split("^")
+            _spin_txt = "Establishing Session with Aruba Central API Gateway..."
+            # with click_spinner.spinner():
+            ses = utils.spinner(_spin_txt, CentralApi)
+            kwargs = {**kwargs, **{"cli_cmds": cmds}}
+
+            _spin_txt = "Sending Commands to Aruba Central API Gateway..."
+            resp = utils.spinner(_spin_txt, ses.caasapi, *args, **kwargs)
+            caas_response(resp)
+
+
+@app.command()
+def test():
+    # var = "XYZ"
+    pass
+    # _pretty(f"This is a {{RED:BOLD:{var}}} test.")
+    # group_dev = "20:4C:03:26:28:4c"
+    # mac = "20:4C:03:26:28:4c"
+    # serial = "CNF7JSP0N0"
+    # # show = "committed"
+    # # cmds = ["vlan 123"]
+    # ses = CentralApi()
+    # # resp = ses.caasapi(group_dev, cmds)
+    # # resp = ses.verify_add_dev(mac, serial)
+    # resp = ses.add_dev(mac, serial)
+
+    # pprint(resp)
+
+    # TODO move command gen to BuildCLI
+    # caas_response(session.caasapi(group_dev, cmds))
+    # for c in cmds:
+    #     typer.echo(c)
+
+
+if __name__ == "__main__":
+    app()
---
+++ lib/centralCLI/utils.py
@@ -0,0 +1,329 @@
+#!/usr/bin/env python3
+
+import string
+import subprocess
+import shlex
+import time
+import os
+import sys
+import yaml
+# import stat
+# import grp
+import json
+# import threading
+import socket
+from io import StringIO
+from halo import Halo
+
+try:
+    loc_user = os.getlogin()
+except Exception:
+    loc_user = os.getenv("SUDO_USER", os.getenv("USER"))
+
+
+class Convert:
+    def __init__(self, mac):
+        self.orig = mac
+        if not mac:
+            mac = '0'
+        self.clean = ''.join([c for c in list(mac) if c in string.hexdigits])
+        self.ok = True if len(self.clean) == 12 else False
+        self.cols = ':'.join(self.clean[i:i+2] for i in range(0, 12, 2))
+        self.dashes = '-'.join(self.clean[i:i+2] for i in range(0, 12, 2))
+        self.dots = '.'.join(self.clean[i:i+4] for i in range(0, 12, 4))
+        self.tag = f"ztp-{self.clean[-4:]}"
+        self.dec = int(self.clean, 16) if self.ok else 0
+
+
+class Mac(Convert):
+    def __init__(self, mac):
+        super().__init__(mac)
+        oobm = hex(self.dec + 1).lstrip('0x')
+        self.oobm = Convert(oobm)
+
+
+class Utils:
+    def __init__(self):
+        self.Mac = Mac
+
+    def user_input_bool(self, question):
+        """Ask User Y/N Question require Y/N answer
+
+        Error and reprompt if user's response is not valid
+        Appends '? (y/n): ' to question/prompt provided
+
+        Params:
+            question:str, The Question to ask
+        Returns:
+            answer:bool, Users Response yes=True
+        """
+        valid_answer = ["yes", "y", "no", "n"]
+        try:
+            answer = input(question + "? (y/n): ").strip()
+        except (KeyboardInterrupt, EOFError):
+            print("")  # prevents header printing on same line when in debug
+            return False
+        while answer.lower() not in valid_answer:
+            if answer != "":
+                print(
+                    f" \033[1;33m!!\033[0m Invalid Response '{answer}' Valid Responses: {valid_answer}"
+                )
+            answer = input(question + "? (y/n): ").strip()
+        if answer[0].lower() == "y":
+            return True
+        else:
+            return False
+
+    def error_handler(self, cmd, stderr, user=loc_user):
+        if isinstance(cmd, str):
+            cmd = shlex.split(cmd)
+        if stderr and "FATAL: cannot lock /dev/" not in stderr:
+            # Handle key change Error
+            if "WARNING: REMOTE HOST IDENTIFICATION HAS CHANGED!" in stderr:
+                print(
+                    "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n"
+                    "@    WARNING: REMOTE HOST IDENTIFICATION HAS CHANGED!     @\n"
+                    "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n"
+                    "IT IS POSSIBLE THAT SOMEONE IS DOING SOMETHING NASTY!\n"
+                    "Someone could be eavesdropping on you right now (man-in-the-middle attack)!\n"
+                    "It is also possible that a host key has just been changed."
+                )
+                while True:
+                    choice = ""
+                    try:
+                        choice = input(
+                            "\nDo you want to remove the old host key and re-attempt the connection (y/n)? "
+                        )
+                        if choice.lower() in ["y", "yes"]:
+                            _cmd = shlex.split(
+                                stderr.replace("\r", "")
+                                .split("remove with:\n")[1]
+                                .split("\n")[0]
+                                .replace("ERROR:   ", "")
+                            )  # NoQA
+                            _cmd = shlex.split("sudo -u {}".format(user)) + _cmd
+                            subprocess.run(_cmd)
+                            print("\n")
+                            subprocess.run(cmd)
+                            break
+                        elif choice.lower() in ["n", "no"]:
+                            break
+                        else:
+                            print(
+                                "\n!!! Invalid selection {} please try again.\n".format(
+                                    choice
+                                )
+                            )
+                    except (KeyboardInterrupt, EOFError):
+                        print("")
+                        return "Aborted last command based on user input"
+                    except ValueError:
+                        print(
+                            f"\n!! Invalid selection {choice} please try again.\n")
+            elif (
+                "All keys were skipped because they already exist on the remote system"
+                in stderr
+            ):
+                return "Skipped - key already exists"
+            elif "/usr/bin/ssh-copy-id: INFO:" in stderr:
+                if "sh: 1:" in stderr:
+                    return "".join(stderr.split("sh: 1:")[1:]).strip()
+            # ssh cipher suite errors
+            elif "no matching cipher found. Their offer:" in stderr:
+                print("Connection Error: {}\n".format(stderr))
+                cipher = stderr.split("offer:")[1].strip().split(",")
+                aes_cipher = [c for c in cipher if "aes" in c]
+                if aes_cipher:
+                    cipher = aes_cipher[-1]
+                else:
+                    cipher = cipher[-1]
+                cmd += ["-c", cipher]
+
+                print("Reattempting Connection using cipher {}".format(cipher))
+                r = subprocess.run(cmd)
+                if r.returncode:
+                    return "Error on Retry Attempt"  # TODO better way... handle banners... paramiko?
+            else:
+                return stderr  # return value that was passed in
+
+        # Handle hung sessions always returncode=1 doesn't always present stderr
+        elif cmd[0] == "picocom":
+            if self.kill_hung_session(cmd[1]):
+                subprocess.run(cmd)
+            else:
+                return "User Abort or Failure to kill existing session to {}".format(
+                    cmd[1].replace("/dev/", "")
+                )
+
+    def shell_output_cleaner(self, output):
+        strip_words = ["/usr/bin/ssh-copy-id: "]
+        return "".join(
+            [x.replace(i, "") for x in self.listify(output) for i in strip_words]
+        )
+
+    def do_shell_cmd(
+        self,
+        cmd,
+        do_print=False,
+        handle_errors=True,
+        return_stdout=False,
+        tee_stderr=False,
+        timeout=5,
+        shell=False,
+        **kwargs,
+    ):
+        """Runs shell cmd (i.e. ssh) returns stderr if any by default
+
+        Arguments:
+            cmd {str|list} -- commands/args sent to subprocess
+
+        Keyword Arguments:
+            do_print {bool} -- Print stderr after cmd completes (default: {False})
+            handle_errors {bool} -- Send stderr to error_handler (default: {True})
+            return_stdout {bool} -- run with shell and return tuple returncode, stdout, stderr (default: {False})
+            tee_stderr {bool} -- stderr is displayed and captured (default: {False})
+            timeout {int} -- subprocess timeout (default: {5})
+            shell {bool} -- run cmd as shell (default: {True})
+
+        Returns:
+            {str|tuple} -- By default there is no return unless there is an error.
+            return_stdout=True will return tuple returncode, stdout, stderr
+        """
+        if return_stdout:
+            res = subprocess.run(
+                cmd,
+                shell=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                universal_newlines=True,
+                **kwargs,
+            )
+            return res.returncode, res.stdout.strip(), res.stderr.strip()
+        elif shell:
+            res = subprocess.run(
+                cmd,
+                shell=True,
+                stderr=subprocess.PIPE,
+                universal_newlines=True,
+                **kwargs,
+            )
+            if res.stderr:
+                return (
+                    res.stderr
+                    if not handle_errors
+                    else self.error_handler(cmd, res.stderr)
+                )
+
+        else:
+            if isinstance(cmd, str):
+                cmd = shlex.split(cmd)
+
+            if tee_stderr:
+                s = subprocess
+                start_time = time.time()
+                with s.Popen(
+                    cmd, stderr=s.PIPE, bufsize=1, universal_newlines=True, **kwargs
+                ) as p, StringIO() as buf1, StringIO() as buf2:
+                    for line in p.stderr:
+                        print(line, end="")
+                        # handles login banners which are returned via stderr
+                        if time.time() - start_time < timeout + 5:
+                            buf1.write(line)
+                        else:
+                            buf2.write(line)
+
+                    early_error = buf1.getvalue()
+                    late_error = buf2.getvalue()
+
+                # if connections lasted 20 secs past timeout assume the early stuff was innocuous
+                if time.time() - start_time > timeout + 10:
+                    error = late_error
+                else:
+                    error = early_error
+
+                if handle_errors and error and p.returncode != 0:
+                    error = self.error_handler(cmd, error)
+                else:
+                    error = None
+                return error
+            else:
+                proc = subprocess.Popen(
+                    cmd, stderr=subprocess.PIPE, universal_newlines=True, **kwargs
+                )
+                err = proc.communicate(timeout=timeout)[1]
+                if err is not None and do_print:
+                    print(self.shell_output_cleaner(err), file=sys.stdout)
+                # if proc.returncode != 0 and handle_errors:
+                if err and handle_errors:
+                    err = self.error_handler(cmd, err)
+
+                proc.wait()
+                return err
+
+    def json_print(self, obj):
+        print(json.dumps(obj, indent=4, sort_keys=True))
+
+    def get_tty_size(self):
+        size = subprocess.run(["stty", "size"], stdout=subprocess.PIPE)
+        rows, cols = size.stdout.decode("UTF-8").split()
+        return int(rows), int(cols)
+
+    # TODO check if using kwarg sort added to fix re-order of error_msgs
+    def unique(self, _list, sort=False):
+        out = []
+        [out.append(i) for i in _list if i not in out and i is not None]
+        return out if not sort else sorted(out)
+
+    def is_reachable(self, host, port, timeout=3, silent=False):
+        s = socket.socket()
+        try:
+            s.settimeout(timeout)
+            s.connect((host, port))
+            _reachable = True
+        except Exception as e:
+            if not silent:
+                print("something's wrong with %s:%d. Exception is %s" % (host, port, e))
+            _reachable = False
+        finally:
+            s.close()
+        return _reachable
+
+    def valid_file(self, filepath):
+        return os.path.isfile(filepath) and os.stat(filepath).st_size > 0
+
+    def listify(self, var):
+        return var if isinstance(var, list) or var is None else [var]
+
+    def read_yaml(self, filename):
+        """Read variables from local yaml file
+
+        :param filename: local yaml file, defaults to 'vars.yaml'
+        :type filename: str
+        :return: Required variables
+        :rtype: Python dictionary
+        """
+        filename = os.path.abspath(os.path.join(os.path.dirname(__file__), filename))
+        with open(filename, "r") as input_file:
+            data = yaml.load(input_file, Loader=yaml.FullLoader)
+        return data
+
+    def get_host_short(self, host):
+        """Extract hostname from fqdn
+
+        Arguments:
+            host {str} -- hostname. If ip address is provided it's returned as is
+
+        Returns:
+            str -- host_short (lab1.example.com becomes lab1)
+        """
+        return (
+            host.split(".")[0]
+            if "." in host and not host.split(".")[0].isdigit()
+            else host
+        )
+
+    def spinner(self, spin_txt, function, *args, **kwargs):
+        spinner = kwargs.get("spinner", "dots")
+        if sys.stdin.isatty():
+            with Halo(text=spin_txt, spinner=spinner):
+                return function(*args, **kwargs)
