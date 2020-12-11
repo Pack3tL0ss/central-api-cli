ab7ccc61cdf79544e3ebfab99b25ec2ec70adc27
Pack3tL0ss <wade99@gmail.com>
2020-12-11 02:06:56 -0600

    cleanup handle list of strings

--- lib/centralCLI/utils.py
+++ lib/centralCLI/utils.py
@@ -1,29 +1,19 @@
 #!/usr/bin/env python3
 
-import string
-import subprocess
-import shutil
-import shlex
-import time
-import os
-import sys
-from typing import Any
-import yaml
-# import stat
-# import grp
 import json
-# import threading
+import os
+import shutil
 import socket
-from io import StringIO
-from halo import Halo
-from tabulate import tabulate
-from pygments import highlight, lexers, formatters
+import string
+import sys
 import urllib.parse
+from pprint import pprint
+from typing import Union
 
-try:
-    loc_user = os.getlogin()
-except Exception:
-    loc_user = os.getenv("SUDO_USER", os.getenv("USER"))
+import yaml
+from halo import Halo
+from pygments import formatters, highlight, lexers
+from tabulate import tabulate
 
 # removed from output and placed at top (provided with each item returned)
 CUST_KEYS = ["customer_id", "customer_name"]
@@ -36,7 +26,9 @@ class Convert:
             mac = '0'
         self.clean = ''.join([c for c in list(mac) if c in string.hexdigits])
         self.ok = True if len(self.clean) == 12 else False
-        self.cols = ':'.join(self.clean[i:i+2] for i in range(0, 12, 2))
+        cols = ':'.join(self.clean[i:i+2] for i in range(0, 12, 2))
+        if cols.strip().endswith(':'):  # handle macs starting with 00 for oobm
+            cols = f"00:{cols.strip().rstrip(':')}"
         self.dashes = '-'.join(self.clean[i:i+2] for i in range(0, 12, 2))
         self.dots = '.'.join(self.clean[i:i+4] for i in range(0, 12, 4))
         self.tag = f"ztp-{self.clean[-4:]}"
@@ -83,194 +75,13 @@ class Utils:
         else:
             return False
 
-    def error_handler(self, cmd, stderr, user=loc_user):
-        if isinstance(cmd, str):
-            cmd = shlex.split(cmd)
-        if stderr and "FATAL: cannot lock /dev/" not in stderr:
-            # Handle key change Error
-            if "WARNING: REMOTE HOST IDENTIFICATION HAS CHANGED!" in stderr:
-                print(
-                    "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n"
-                    "@    WARNING: REMOTE HOST IDENTIFICATION HAS CHANGED!     @\n"
-                    "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n"
-                    "IT IS POSSIBLE THAT SOMEONE IS DOING SOMETHING NASTY!\n"
-                    "Someone could be eavesdropping on you right now (man-in-the-middle attack)!\n"
-                    "It is also possible that a host key has just been changed."
-                )
-                while True:
-                    choice = ""
-                    try:
-                        choice = input(
-                            "\nDo you want to remove the old host key and re-attempt the connection (y/n)? "
-                        )
-                        if choice.lower() in ["y", "yes"]:
-                            _cmd = shlex.split(
-                                stderr.replace("\r", "")
-                                .split("remove with:\n")[1]
-                                .split("\n")[0]
-                                .replace("ERROR:   ", "")
-                            )  # NoQA
-                            _cmd = shlex.split("sudo -u {}".format(user)) + _cmd
-                            subprocess.run(_cmd)
-                            print("\n")
-                            subprocess.run(cmd)
-                            break
-                        elif choice.lower() in ["n", "no"]:
-                            break
-                        else:
-                            print(
-                                "\n!!! Invalid selection {} please try again.\n".format(
-                                    choice
-                                )
-                            )
-                    except (KeyboardInterrupt, EOFError):
-                        print("")
-                        return "Aborted last command based on user input"
-                    except ValueError:
-                        print(
-                            f"\n!! Invalid selection {choice} please try again.\n")
-            elif (
-                "All keys were skipped because they already exist on the remote system"
-                in stderr
-            ):
-                return "Skipped - key already exists"
-            elif "/usr/bin/ssh-copy-id: INFO:" in stderr:
-                if "sh: 1:" in stderr:
-                    return "".join(stderr.split("sh: 1:")[1:]).strip()
-            # ssh cipher suite errors
-            elif "no matching cipher found. Their offer:" in stderr:
-                print("Connection Error: {}\n".format(stderr))
-                cipher = stderr.split("offer:")[1].strip().split(",")
-                aes_cipher = [c for c in cipher if "aes" in c]
-                if aes_cipher:
-                    cipher = aes_cipher[-1]
-                else:
-                    cipher = cipher[-1]
-                cmd += ["-c", cipher]
-
-                print("Reattempting Connection using cipher {}".format(cipher))
-                r = subprocess.run(cmd)
-                if r.returncode:
-                    return "Error on Retry Attempt"  # TODO better way... handle banners... paramiko?
-            else:
-                return stderr  # return value that was passed in
-
-        # Handle hung sessions always returncode=1 doesn't always present stderr
-        elif cmd[0] == "picocom":
-            if self.kill_hung_session(cmd[1]):
-                subprocess.run(cmd)
-            else:
-                return "User Abort or Failure to kill existing session to {}".format(
-                    cmd[1].replace("/dev/", "")
-                )
-
-    def shell_output_cleaner(self, output):
-        strip_words = ["/usr/bin/ssh-copy-id: "]
-        return "".join(
-            [x.replace(i, "") for x in self.listify(output) for i in strip_words]
-        )
-
-    def do_shell_cmd(
-        self,
-        cmd,
-        do_print=False,
-        handle_errors=True,
-        return_stdout=False,
-        tee_stderr=False,
-        timeout=5,
-        shell=False,
-        **kwargs,
-    ):
-        """Runs shell cmd (i.e. ssh) returns stderr if any by default
-
-        Arguments:
-            cmd {str|list} -- commands/args sent to subprocess
-
-        Keyword Arguments:
-            do_print {bool} -- Print stderr after cmd completes (default: {False})
-            handle_errors {bool} -- Send stderr to error_handler (default: {True})
-            return_stdout {bool} -- run with shell and return tuple returncode, stdout, stderr (default: {False})
-            tee_stderr {bool} -- stderr is displayed and captured (default: {False})
-            timeout {int} -- subprocess timeout (default: {5})
-            shell {bool} -- run cmd as shell (default: {True})
-
-        Returns:
-            {str|tuple} -- By default there is no return unless there is an error.
-            return_stdout=True will return tuple returncode, stdout, stderr
-        """
-        if return_stdout:
-            res = subprocess.run(
-                cmd,
-                shell=True,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                universal_newlines=True,
-                **kwargs,
-            )
-            return res.returncode, res.stdout.strip(), res.stderr.strip()
-        elif shell:
-            res = subprocess.run(
-                cmd,
-                shell=True,
-                stderr=subprocess.PIPE,
-                universal_newlines=True,
-                **kwargs,
-            )
-            if res.stderr:
-                return (
-                    res.stderr
-                    if not handle_errors
-                    else self.error_handler(cmd, res.stderr)
-                )
-
-        else:
-            if isinstance(cmd, str):
-                cmd = shlex.split(cmd)
-
-            if tee_stderr:
-                s = subprocess
-                start_time = time.time()
-                with s.Popen(
-                    cmd, stderr=s.PIPE, bufsize=1, universal_newlines=True, **kwargs
-                ) as p, StringIO() as buf1, StringIO() as buf2:
-                    for line in p.stderr:
-                        print(line, end="")
-                        # handles login banners which are returned via stderr
-                        if time.time() - start_time < timeout + 5:
-                            buf1.write(line)
-                        else:
-                            buf2.write(line)
-
-                    early_error = buf1.getvalue()
-                    late_error = buf2.getvalue()
-
-                # if connections lasted 20 secs past timeout assume the early stuff was innocuous
-                if time.time() - start_time > timeout + 10:
-                    error = late_error
-                else:
-                    error = early_error
-
-                if handle_errors and error and p.returncode != 0:
-                    error = self.error_handler(cmd, error)
-                else:
-                    error = None
-                return error
-            else:
-                proc = subprocess.Popen(
-                    cmd, stderr=subprocess.PIPE, universal_newlines=True, **kwargs
-                )
-                err = proc.communicate(timeout=timeout)[1]
-                if err is not None and do_print:
-                    print(self.shell_output_cleaner(err), file=sys.stdout)
-                # if proc.returncode != 0 and handle_errors:
-                if err and handle_errors:
-                    err = self.error_handler(cmd, err)
-
-                proc.wait()
-                return err
-
     def json_print(self, obj):
-        print(json.dumps(obj, indent=4, sort_keys=True))
+        try:
+            ret = json.dumps(obj, indent=4, sort_keys=True)
+        except Exception:
+            ret = pprint(obj, indent=4, sort_dicts=True)
+        finally:
+            print(ret)
 
     class TTY:
         def __init__(self):
@@ -292,13 +103,14 @@ class Utils:
 
     tty = TTY()
 
-    # TODO check if using kwarg sort added to fix re-order of error_msgs
-    def unique(self, _list, sort=False):
+    @staticmethod
+    def unique(_list: list, sort: bool = False) -> list:
         out = []
         [out.append(i) for i in _list if i not in out and i is not None]
         return out if not sort else sorted(out)
 
-    def is_reachable(self, host, port, timeout=3, silent=False):
+    @staticmethod
+    def is_reachable(host: str, port: Union[str, list], timeout: int = 3, silent: bool = False):
         s = socket.socket()
         try:
             s.settimeout(timeout)
@@ -364,11 +176,12 @@ class Utils:
             return len(str(self.file).splitlines())
 
         def __str__(self):
-            return self.tty
+            return self.tty or self.file
 
     def output(self, outdata, tablefmt):
         # log.debugv(f"data passed to output():\n{pprint(outdata, indent=4)}")
-        _lexer = raw_data = table_data = None
+        raw_data = outdata
+        _lexer = table_data = None
         if tablefmt == "json":
             # from pygments import highlight, lexers, formatters
             raw_data = json.dumps(outdata, sort_keys=True, indent=2)
@@ -390,14 +203,16 @@ class Utils:
         else:
             customer_id = customer_name = ""
             outdata = self.listify(outdata)
-            if outdata and isinstance(outdata, list) and isinstance(outdata[0], dict):
+            if outdata and all(isinstance(x, dict) for x in outdata):  # verify List[dict, ...]
                 customer_id = outdata[0].get("customer_id", "")
                 customer_name = outdata[0].get("customer_name", "")
-            outdata = [{k: v for k, v in d.items() if k not in CUST_KEYS} for d in outdata]
-            table_data = tabulate(outdata, headers="keys", tablefmt=tablefmt)
-            raw_data = table_data = f"--\n{'Customer ID:':15}{customer_id}\n" \
-                                    f"{'Customer Name:':15} {customer_name}\n--\n" \
-                                    f"{table_data}"
+                outdata = [{k: v for k, v in d.items() if k not in CUST_KEYS} for d in outdata]
+                table_data = tabulate(outdata, headers="keys", tablefmt=tablefmt)
+                raw_data = table_data = f"--\n{'Customer ID:':15}{customer_id}\n" \
+                                        f"{'Customer Name:':15} {customer_name}\n--\n" \
+                                        f"{table_data}"
+            elif outdata and (isinstance(x, str) for x in outdata):  # verify List[dict, ...]
+                raw_data = table_data = "{}{}{}".format("--\n", '\n'.join(outdata), "\n--")
 
         if _lexer and raw_data:
             table_data = highlight(bytes(raw_data, 'UTF-8'),
