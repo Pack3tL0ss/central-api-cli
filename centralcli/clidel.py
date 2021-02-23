6926f0164981958ded42d95782f6775ce0e50bcd
Pack3tL0ss <wade99@gmail.com>
2021-02-23 13:34:54 -0600

    delete command level

---
+++ centralcli/clidel.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+import sys
+import typer
+
+# Detect if called from pypi installed package or via cloned github repo (development)
+try:
+    from centralcli import cli
+except (ImportError, ModuleNotFoundError) as e:
+    pkg_dir = Path(__file__).absolute().parent
+    if pkg_dir.name == "centralcli":
+        sys.path.insert(0, str(pkg_dir.parent))
+        from centralcli import cli
+    else:
+        print(pkg_dir.parts)
+        raise e
+
+# from centralcli.constants import IdenMetaVars
+
+app = typer.Typer()
+
+
+@app.command(short_help="Delete a certificate")
+def certificate(
+    name: str = typer.Argument(..., ),
+    yes: bool = typer.Option(False, "-Y", help="Bypass confirmation prompts - Assume Yes"),
+    yes_: bool = typer.Option(False, "-y", hidden=True),
+    debug: bool = typer.Option(False, "--debug", envvar="ARUBACLI_DEBUG", help="Enable Additional Debug Logging",
+                               callback=cli.debug_callback),
+    default: bool = typer.Option(False, "-d", is_flag=True, help="Use default central account",
+                                 callback=cli.default_callback),
+    account: str = typer.Option("central_info",
+                                envvar="ARUBACLI_ACCOUNT",
+                                help="The Aruba Central Account to use (must be defined in the config)",
+                                callback=cli.account_name_callback),
+) -> None:
+    yes = yes_ if yes_ else yes
+    confirm_1 = typer.style("Please Confirm:", fg="cyan")
+    confirm_2 = typer.style("Delete", fg="bright_red")
+    confirm_3 = typer.style(f"certificate {name}", fg="cyan")
+    if yes or typer.confirm(f"{confirm_1} {confirm_2} {confirm_3}"):
+        resp = cli.central.request(cli.central.delete_certificate, name)
+        typer.secho(str(resp), fg="green" if resp else "red")
+
+
+@app.command(short_help="Delete a site")
+def site(
+    name: str = typer.Argument(...,),  # metavar=IdenMetaVars.site),
+    yes: bool = typer.Option(False, "-Y", help="Bypass confirmation prompts - Assume Yes"),
+    yes_: bool = typer.Option(False, "-y", hidden=True),
+    debug: bool = typer.Option(False, "--debug", envvar="ARUBACLI_DEBUG", help="Enable Additional Debug Logging",
+                               callback=cli.debug_callback),
+    default: bool = typer.Option(False, "-d", is_flag=True, help="Use default central account",
+                                 callback=cli.default_callback),
+    account: str = typer.Option("central_info",
+                                envvar="ARUBACLI_ACCOUNT",
+                                help="The Aruba Central Account to use (must be defined in the config)",
+                                callback=cli.account_name_callback),
+) -> None:
+    site = cli.cache.get_site_identifier(name)
+    confirm_1 = typer.style("Please Confirm:", fg="cyan")
+    confirm_2 = typer.style("Delete", fg="bright_red")
+    confirm_3 = typer.style(f"site {name}", fg="cyan")
+    if yes or typer.confirm(f"{confirm_1} {confirm_2} {confirm_3}"):
+        resp = cli.central.request(cli.central.delete_site, site.id)
+        typer.secho(str(resp), fg="green" if resp else "red")
+
+
+@app.callback()
+def callback():
+    """
+    Delete Aruba Central Objects.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2 and sys.argv[1] == 'delete':
+        if sys.argv[2] == "sites":
+            sys.argv[2] = "site"
+        elif sys.argv[2] in ["certificates", "certs", "cert"]:
+            sys.argv[2] = "certificate"
+
+    app()
