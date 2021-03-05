a572dabbcf2e5ba50c3904da71bcd19196e287a5
Pack3tL0ss <wade99@gmail.com>
2021-03-05 15:33:30 -0600

    add client counts to caption

--- centralcli/clishow.py
+++ centralcli/clishow.py
@@ -842,6 +842,10 @@ def clients(
         args = (filter.value, *args)
 
     resp = central.request(central.get_clients, *args, **kwargs)
+    _tot = len(resp)
+    _wired = len([x for x in resp.output if x["client_type"] == "WIRED"])
+    _wireless = len([x for x in resp.output if x["client_type"] == "WIRELESS"])
+    _count_text = f"{_tot} Clients, (Wired: {_wired}, Wireless: {_wireless})."
 
     _format = "rich" if not verbose and not verbose2 else "yaml"
     tablefmt = cli.get_format(do_json, do_yaml, do_csv, do_table, default=_format)
@@ -856,7 +860,7 @@ def clients(
         resp,
         tablefmt=tablefmt,
         title="Clients",
-        caption="Use -v for more details, -vv for unformatted response." if not verbose else None,
+        caption=f"{_count_text} Use -v for more details, -vv for unformatted response." if not verbose else None,
         pager=not no_pager,
         outfile=outfile,
         reverse=reverse,
