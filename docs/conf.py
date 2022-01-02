b8038f3227c2d1c0f3161de635bbf3518620f878
Wade ~ Pack3tL0ss <Use github to contact>
2022-01-02 14:13:18 -0600

    use m2r2 for md inclusion

--- docs/conf.py
+++ docs/conf.py
@@ -31,8 +31,9 @@ author = 'Wade Wells ~ Pack3tL0ss'
 # ones.
 extensions = [
     'sphinx_rtd_theme', 'sphinx.ext.autodoc', 'sphinx.ext.coverage',
-    'sphinx.ext.autosummary', 'recommonmark', 'sphinx_click'
+    'sphinx.ext.autosummary', 'm2r2', 'sphinx_click'
 ]
+source_suffix = [".rst", ".md"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
