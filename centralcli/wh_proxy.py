dbd5efae035a41ff131e1ee2fe5bd9583b0e98cf
Pack3tL0ss <wade99@gmail.com>
2022-01-14 17:22:27 -0600

    wh_proxy logging

--- centralcli/wh_proxy.py
+++ centralcli/wh_proxy.py
@@ -135,15 +135,19 @@ async def webhook(
             del_id=x_central_delivery_id,
         ):
             # print_json(data=raw_input)
+            remove = False
             if data.get("state"):
                 if data["state"] == "Open":
                     log.info(f"Caching incoming alert {data.get('alert_type', '')} for device {data.get('device_id', '')}")
                 else:
                     log.info(f"Clearing alert {data.get('alert_type', '')} for device {data.get('device_id', '')}")
+                    remove = True
             else:
                 log.warning("State Attribute not found in Incoming data")
 
-            await cache.update_hook_data_db(data)
+            cache_res = await cache.update_hook_data_db(data)
+            if remove:
+                log.info(f"removed doc_id: {cache_res}")
             return {"result": "ok"}
         else:
             log.error("POST from Central Rcvd with Invalid signature", show=False)
