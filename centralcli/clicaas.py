2101eaf467c883f99d2152f4b3c0de3c2f51b387
Pack3tL0ss <wade99@gmail.com>
2022-02-04 00:38:05 -0600

    add WIP send_cmds

--- centralcli/clicaas.py
+++ centralcli/clicaas.py
@@ -28,23 +28,29 @@ Then run via
   cencli batch add-vlan addvlan10  [--file <alternate import file>]
 
 """
+# from enum import auto
 from pathlib import Path
 import sys
+from threading import get_ident
+from rich import print
 import typer
+from typing import List
+from rich.console import Console
 
 # Detect if called from pypi installed package or via cloned github repo (development)
 try:
-    from centralcli import cli, config, utils, caas
+    from centralcli import cli, config, utils, caas, constants, cleaner
 except (ImportError, ModuleNotFoundError) as e:
     pkg_dir = Path(__file__).absolute().parent
     if pkg_dir.name == "centralcli":
         sys.path.insert(0, str(pkg_dir.parent))
-        from centralcli import cli, config, utils, caas
+        from centralcli import cli, config, utils, caas, constants, cleaner
     else:
         print(pkg_dir.parts)
         raise e
 
 tty = utils.tty
+iden = constants.IdenMetaVars()
 app = typer.Typer()
 SPIN_TXT_CMDS = "Sending Commands to Aruba Central API Gateway..."
 
@@ -76,7 +82,8 @@ def bulk_edit(
             raise typer.Abort()
 
 
-@app.command()
+# FIXME
+@app.command(hidden=True)
 def add_vlan(
     group_dev: str = typer.Argument(...),
     pvid: str = typer.Argument(...),
@@ -228,10 +235,125 @@ def caas_batch(
             caas.eval_caas_response(resp)
 
 
+@app.command(help="Send commands to gateway(s) (group or device level)", short_help="Send commands to gateways", hidden=True)
+def send_cmds(
+    device: str = typer.Argument(
+        None,
+        autocompletion=cli.cache.dev_gw_completion,
+        hidden=True,
+        is_eager=True,
+    ),
+    commands: List[str] = typer.Argument(None),
+    cmd_file: Path = typer.Option(None, help="Path to file containing commands (1 per line) to be sent to device", exists=True),
+    dev_file: Path = typer.Option(None, help="Path to file containing iden for devices to send commands to", exists=True),
+    # devices: List[str] = typer.Option(
+    #     None,
+    #     metavar=iden.dev,
+    #     autocompletion=cli.cache.dev_gw_completion,
+    #     help="Gateways to send commands to",
+    #     is_eager=True,
+    # ),
+    group: str = typer.Option(None, help="Send commands to all gateways in a group", autocompletion=cli.cache.group_completion),
+    site: str = typer.Option(None, help="Send commands to all gateways in a site", autocompletion=cli.cache.site_completion),
+    # file: Path = typer.Option(None, exists=True,),
+    # all: bool = typer.Option(False, "--all", help="Send command to all gateways in the site or group.  Requires --site or --group option"),
+    yes: bool = typer.Option(False, "-Y", help="Bypass confirmation prompts - Assume Yes"),
+    yes_: bool = typer.Option(False, "-y", hidden=True),
+    default: bool = typer.Option(False, "-d", is_flag=True, help="Use default central account",
+                                 callback=cli.default_callback),
+    debug: bool = typer.Option(False, "--debug", envvar="ARUBACLI_DEBUG", help="Enable Additional Debug Logging",
+                               callback=cli.debug_callback),
+    account: str = typer.Option("central_info",
+                                envvar="ARUBACLI_ACCOUNT",
+                                help="The Aruba Central Account to use (must be defined in the config)",
+                                callback=cli.account_name_callback),
+) -> None:
+    console = Console(emoji=False)
+    yes = yes if yes else yes_
+    commands = commands or []
+    if dev_file and device:
+        commands.insert(0, device)
+        device = None
+    elif group or site and device:
+        commands = [device, *commands]
+        device = None
+    # devices = devices if devices else device
+    error = None
+    if not commands and (not cmd_file and dev_file and not device):
+        error = "Error: No commands provided, provide as arguments or with --cmd-file <Path> option."
+    elif commands and cmd_file:
+        error = "Invalid combination, provide commands as arguments or --cmd-file <path> not both."
+    if all([x is None for x in [device, dev_file, group, site]]):
+        error = "Error: You must provide one --device --dev-file <Path> --group <group> --site <site>"
+    if site and group:
+        error = "Error: Invalid combination, provide only 1 of --site <site> --group <group>"
+
+    if error:
+        print(error)
+        raise typer.Exit(1)
+
+    # Determine devices to send commands to.
+    all_group, all_site = False, False
+    if dev_file:
+        devices = [
+            cli.cache.get_dev_identifier(d, dev_type="gw") for d in dev_file.read_text().splitlines()
+        ]
+    if device:
+        # devices = [cli.cache.get_dev_identifier(d, dev_type="gw") for d in devices]
+        devices = [cli.cache.get_dev_identifier(device, dev_type="gw")]
+    if group:
+        group = cli.cache.get_group_identifier(group)
+        devices = [d for d in cli.cache.devices if d["type"] == "gw" and d["group"] == group.name]
+        all_group = True
+        # if devices:
+        #     devices = [d for d in devices if d.group == group.name]
+        # else:
+        #     devices = [d for d in cli.cache.devices if d.generic_type == "gw" and d.group == group.name]
+        #     all_group = True
+    if site:
+        site = cli.cache.get_site_identifier(site)
+        devices = [d for d in cli.cache.devices if d["type"] == "gw" and d["site"] == site.name]
+        all_site = True
+        # if devices:
+        #     devices = [d for d in devices if d.site == site.name]
+        # else:
+        #     devices = [d for d in cli.cache.devices if d.generic_type == "gw" and d.site == site.name]
+        #     all_site = True
+
+    if not devices:
+        print(f"Error: This command has a logic error, no devices based on current options")
+        raise typer.Exit(1)
+
+    if cmd_file:
+        commands = cmd_file.read_text().splitlines()
+
+    _cmds = "\n".join(commands)
+    console.print()
+    console.rule(f"Sending the following commands")
+    console.print(f"[bright_green]{_cmds}")
+    console.rule()
+    if all_group:
+        print(f"To all {len(devices)} gateways in {group.name} group.")
+    elif all_site:
+        print(f"To all {len(devices)} gateways in {site.name} branch.")
+    else:
+        print(f"\nTo the following {len(devices)} Gateways")
+        print(
+            "\n".join([f"[cyan]{d.name}[/cyan]|{typer.unstyle(d.help_text).replace('GW|', '')}" for d in devices])
+        )
+    if yes or typer.confirm("\nProceed?", abort=True):
+        caasapi = caas.CaasAPI(central=cli.central)
+        _reqs = [cli.central.BatchRequest(caasapi.send_commands, dev["mac"], cli_cmds=commands) for dev in devices]
+        batch_res = cli.central.batch_request(_reqs)
+        cli.display_results(batch_res, cleaner=cleaner.parse_caas_response)
+        # caas.
+        # Rich progress bar here
+
+
 @app.callback()
 def callback():
     """
-    Interact with Aruba Central CAAS API
+    Interact with Aruba Central CAAS API (Gateways)
     """
     pass
 
