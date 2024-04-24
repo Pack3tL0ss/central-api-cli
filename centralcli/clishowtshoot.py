04e5ade4526a34af1b8604c60de2334eb8df68df
wade ~ Pack3tL0ss <wade99 at github>
2024-04-24 14:48:20 -0400

    :coffin: Remove unused imports :lipstick: Improve help text

--- centralcli/clishowtshoot.py
+++ centralcli/clishowtshoot.py
@@ -2,41 +2,30 @@
 # -*- coding: utf-8 -*-
 
 import typer
-import time
-import pendulum
-import asyncio
 import sys
-from typing import List, Union
 from pathlib import Path
 from rich import print
 from rich.console import Console
 
-try:
-    import psutil
-    hook_enabled = True
-except (ImportError, ModuleNotFoundError):
-    hook_enabled = False
-
 
 # Detect if called from pypi installed package or via cloned github repo (development)
 try:
-    from centralcli import cleaner, cli, utils
+    from centralcli import cleaner, cli
 except (ImportError, ModuleNotFoundError) as e:
     pkg_dir = Path(__file__).absolute().parent
     if pkg_dir.name == "centralcli":
         sys.path.insert(0, str(pkg_dir.parent))
-        from centralcli import cleaner, cli, utils
+        from centralcli import cleaner, cli
     else:
         print(pkg_dir.parts)
         raise e
 
 from centralcli.constants import (
-    IdenMetaVars, DevTypes, SortTsCmdOptions, TSDevTypes, lib_to_api  # noqa
+    IdenMetaVars, SortTsCmdOptions, TSDevTypes, lib_to_api  # noqa
 )
 
 app = typer.Typer()
 
-tty = utils.tty
 iden_meta = IdenMetaVars()
 
 
@@ -47,10 +36,12 @@ def results(
         metavar=iden_meta.dev,
         help="Aruba Central Device",
         autocompletion=cli.cache.dev_completion,
+        show_default=False,
     ),
     session_id: str = typer.Argument(
         None,
         help="The troubleshooting session id.",
+        show_default=False,
     ),
     verbose2: bool = typer.Option(
         False,
@@ -151,7 +142,6 @@ def commands(
     if sort_by == "id":
         sort_by = None
     central = cli.central
-    con = Console(emoji=False)
     dev_type = lib_to_api("tshoot", device_type)
 
     resp = central.request(central.get_ts_commands, device_type=dev_type,)
