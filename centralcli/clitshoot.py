8f6e000f7824665d6768f563ca2d7e6304f361bc
wade ~ Pack3tL0ss <wade99 at github>
2024-04-25 09:10:50 -0500

    :sparkles: Add more tshoot commands :lipstick: Simplify improve `send command` :sparkles: + `show images`

--- centralcli/clitshoot.py
+++ centralcli/clitshoot.py
@@ -73,9 +73,45 @@ def send_cmds_by_id(device: CentralObject, commands: List[int], pager: bool = Fa
                 cli.display_results(ts_resp, tablefmt="action", pager=pager, outfile=outfile)
                 break
 
+def ts_send_command(device: CentralObject, cmd: str, outfile: Path, pager: bool,) -> None:
+    """Helper command to send troubleshooting output (user provides command) and print results
+
+    Args:
+        device (CentralObject): Device Object
+        cmd (str): User provided command
+        outfile (Path): Optional output to file
+        pager (bool): Optional Use Pager
+    """
+    dev: CentralObject = cli.cache.get_dev_identifier(device)
+    dev_type = lib_to_api("tshoot", dev.type)
+    if len(cmd) == 1:
+        cmd = cmd[0].split()
+    cmd = " ".join(cmd)
+    cmd = cmd.replace("  ", " ").strip().lower()
+    resp = cli.central.request(cli.central.get_ts_commands, dev_type)
+    if not resp:
+        print('[bright_red]Unable to get troubleshooting command list')
+        cli.display_results(resp)
+    else:
+        cmd_list = resp.output
+        cmd_id = [c["command_id"] for c in cmd_list if c["command"].strip() == cmd]
+        if not cmd_id:
+            if FUZZ:
+                fuzz_match, fuzz_confidence = process.extract(cmd, [c["command"].strip() for c in cmd_list], limit=1)[0]
+                print(f"[bright_red]{cmd}[/] is not a valid troubleshooting command (supported by API) for {dev.type}.")
+                confirm_str = render.rich_capture(f"Did you mean [green3]{fuzz_match}[/]?")
+                if fuzz_confidence >= 70 and typer.confirm(confirm_str):
+                    cmd_id = [c["command_id"] for c in cmd_list if c["command"].strip() == fuzz_match]
+
+        if not cmd_id:
+            caption = f'[bright_red]Error[/]: [cyan]{cmd}[/] not found in available troubleshooting commands for {dev.type}. See available commands above.'
+            cli.display_results(resp, tablefmt="rich", caption=caption, title=f"Available troubleshooting commands for {dev.type}", cleaner=cleaner.show_ts_commands)
+        else:
+            send_cmds_by_id(dev, commands=cmd_id, pager=pager, outfile=outfile)
+
 @app.command()
-def ap_overlay(
-    device: str = typer.Argument(..., metavar=iden_meta.dev, autocompletion=cli.cache.dev_ap_completion, show_default=False,),
+def overlay(
+    device: str = typer.Argument(..., metavar=iden_meta.dev, autocompletion=cli.cache.dev_completion, show_default=False,),
     outfile: Path = typer.Option(None, "--out", help="Output to file (and terminal)", writable=True, show_default=False,),
     pager: bool = typer.Option(False, "--pager", help="Enable Paged Output"),
     default: bool = typer.Option(False, "-d", is_flag=True, help="Use default central account", show_default=False,),
@@ -85,16 +121,42 @@ def ap_overlay(
                                 help="The Aruba Central Account to use (must be defined in the config)",
                                 autocompletion=cli.cache.account_completion),
 ):
-    """Show AP Overlay details (Tunneled SSIDs)
+    """Show GW or AP Overlay details (Tunneled SSIDs) or AOS-SW User Based Tunneling
 
-    [cyan]Returns the output of the following commands useful in troubleshooting overlay AP / gateway tunnels.[/]
+    [cyan]Returns the output of the following commands useful in troubleshooting overlay AP / gateway / ubt tunnels.[/]
 
+    [bright_green]APs[/]
+    [cyan]-[/] show ata current-cfg
+    [cyan]-[/] show overlay tunnel config
     [cyan]-[/] show ata endpoint
-    [cyan]-[/] show show overlay tunnel config
+    [cyan]-[/] show crypto ipsec stats
+    [cyan]-[/] show datapath bridge
     [cyan]-[/] show overlay ssid-cluster status
+
+    [bright_green]GWs[/]
+    [cyan]-[/] show crypto oto
+    [cyan]-[/] show crypto ipsec
+    [cyan]-[/] show crypto-local ipsec-map
+    [cyan]-[/] show tunnelmgr tunnel-list
+    [cyan]-[/] show tunnelmgr counters
+
+    [bright_green]AOS-SW (User Based Tunneling)[/]
+    [cyan]-[/] show tunneled-node-server
+    [cyan]-[/] show tunneled node server state
+    [cyan]-[/] show tunneled node server statistics
+    [cyan]-[/] show tunneled-node-users all
     """
-    dev = cli.cache.get_dev_identifier(device, dev_type=("ap"))
-    commands = [201, 203, 218]
+    ids_by_dev_type = {
+        "ap": [208, 203, 201, 300, 39, 218],
+        "gw": [2453, 2131, 2441, 2454, 2455],
+        "sw": [1189, 1195, 1196, 1191]
+    }
+    dev = cli.cache.get_dev_identifier(device)
+    if dev.type == "cx":
+        print(":warning:  Command not supported on CX switches.")
+        raise typer.Exit(1)
+
+    commands = ids_by_dev_type[dev.type]
     send_cmds_by_id(dev, commands=commands, pager=pager, outfile=outfile)
 
 @app.command()
@@ -139,6 +201,7 @@ def clients(
     dev: CentralObject = cli.cache.get_dev_identifier(device)
     if dev.type == "cx":
         print(":warning:  Command not supported on CX switches.")
+        raise typer.Exit(1)
 
     commands = ids_by_dev_type[dev.type]
     if wired:
@@ -151,8 +214,8 @@ def clients(
 
 
 @app.command()
-def gw_overlay(
-    device: str = typer.Argument(..., metavar=iden_meta.dev, autocompletion=cli.cache.dev_gw_completion, show_default=False,),
+def dpi(
+    device: str = typer.Argument(..., metavar=iden_meta.dev, autocompletion=cli.cache.dev_ap_gw_completion, show_default=False,),
     outfile: Path = typer.Option(None, "--out", help="Output to file (and terminal)", writable=True, show_default=False,),
     pager: bool = typer.Option(False, "--pager", help="Enable Paged Output"),
     default: bool = typer.Option(False, "-d", is_flag=True, help="Use default central account", show_default=False,),
@@ -162,23 +225,33 @@ def gw_overlay(
                                 help="The Aruba Central Account to use (must be defined in the config)",
                                 autocompletion=cli.cache.account_completion),
 ):
-    """Show GW Overlay details (Tunneled SSIDs)
+    """Show DPI output (valid on APs or GWs)
 
-    [cyan]Returns the output of the following commands useful in troubleshooting overlay AP / gateway tunnels.[/]
+    [cyan]Returns the output of the following DPI related commands.[/]
 
-    [cyan]-[/] show crypto oto
-    [cyan]-[/] show crypto ipsec
-    [cyan]-[/] show tunnelmgr tunnel-list
-    [cyan]-[/] show tunnelmgr counters
+    [bright_green]APs[/]
+    [cyan]-[/] show dpi-stats session
+    [cyan]-[/] show dpi debug status
+    [cyan]-[/] show datapath session dpi
+    [cyan]-[/] show datapath dpi-classification-cache
+    [cyan]-[/] show dpi-classification-cache
 
+    [bright_green]GWs[/]
+    [cyan]-[/] show datapath session dpi table
+    [cyan]-[/] show datapath session dpi counters
+    [cyan]-[/] show dpi application all
     """
-    dev = cli.cache.get_dev_identifier(device, dev_type=("gw"))
-    commands = [2453, 2131, 2454, 2455]
+    ids_by_dev_type = {
+        "ap": [190, 210, 211, 317, 318],
+        "gw": [2394, 2395, 2553]
+    }
+    dev: CentralObject = cli.cache.get_dev_identifier(device, dev_type=("ap", "gw"))
+    commands = ids_by_dev_type[dev.type]
     send_cmds_by_id(dev, commands=commands, pager=pager, outfile=outfile)
 
 
 @app.command()
-def ap_dpi(
+def ap_ssid(
     device: str = typer.Argument(..., metavar=iden_meta.dev, autocompletion=cli.cache.dev_ap_completion, show_default=False,),
     outfile: Path = typer.Option(None, "--out", help="Output to file (and terminal)", writable=True, show_default=False,),
     pager: bool = typer.Option(False, "--pager", help="Enable Paged Output"),
@@ -189,18 +262,17 @@ def ap_dpi(
                                 help="The Aruba Central Account to use (must be defined in the config)",
                                 autocompletion=cli.cache.account_completion),
 ):
-    """Show DPI (valid on APs)
+    """Show output of commands to help troubleshoot SSIDs/broadcasting (APs Only)
 
-    [cyan]Returns the output of the following DPI related commands.[/]
+    [cyan]Returns the output of the following commands.[/]
 
-    [cyan]-[/] show dpi-stats session
-    [cyan]-[/] show dpi debug status
-    [cyan]-[/] show datapath session dpi
-    [cyan]-[/] show datapath dpi-classification-cache
-    [cyan]-[/] show dpi-classification-cache
+    [cyan]-[/] show aps
+    [cyan]-[/] show ap bss-table
+    [cyan]-[/] show cluster bss-table
+    [cyan]-[/] show ap-env
     """
-    dev = cli.cache.get_dev_identifier(device, dev_type=("ap"))
-    commands = [190, 210, 211, 317, 318]
+    dev: CentralObject = cli.cache.get_dev_identifier(device, dev_type=("ap"))
+    commands = [4, 24, 177, 53]
     send_cmds_by_id(dev, commands=commands, pager=pager, outfile=outfile)
 
 
@@ -228,6 +300,47 @@ def show_tech(
         "sw": [1032],
         "gw": [2408],
         "cx": [6001]
+    }
+    commands = ids_by_dev_type[dev.type]
+    send_cmds_by_id(dev, commands=commands, pager=pager, outfile=outfile)
+
+
+@app.command()
+def images(
+    device: str = typer.Argument(..., metavar=iden_meta.dev, autocompletion=cli.cache.dev_completion, show_default=False,),
+    outfile: Path = typer.Option(None, "--out", help="Output to file (and terminal)", writable=True, show_default=False,),
+    pager: bool = typer.Option(False, "--pager", help="Enable Paged Output"),
+    default: bool = typer.Option(False, "-d", is_flag=True, help="Use default central account", show_default=False,),
+    debug: bool = typer.Option(False, "--debug", envvar="ARUBACLI_DEBUG", help="Enable Additional Debug Logging",),
+    account: str = typer.Option("central_info",
+                                envvar="ARUBACLI_ACCOUNT",
+                                help="The Aruba Central Account to use (must be defined in the config)",
+                                autocompletion=cli.cache.account_completion),
+):
+    """Show image versions
+
+    [cyan]Returns the output of the following image related commands.[/]
+
+    [bright_green]APs[/]
+    [cyan]-[/] show version
+    [cyan]-[/] show image version
+
+    [bright_green]GWs[/]
+    [cyan]-[/] show version
+    [cyan]-[/] show image version
+
+    [bright_green]AOS-SW[/]
+    [cyan]-[/] show flash
+    """
+    dev: CentralObject = cli.cache.get_dev_identifier(device)
+    if dev.type == "cx":
+        print(":warning:  Command not supported on CX switches.")
+        raise typer.Exit(1)
+
+    ids_by_dev_type = {
+        "ap": [119, 213],
+        "gw": [2002, 2007],
+        "sw": [1046]
     }
     commands = ids_by_dev_type[dev.type]
     send_cmds_by_id(dev, commands=commands, pager=pager, outfile=outfile)
@@ -324,66 +437,6 @@ def ping(
                 cli.display_results(ts_resp, tablefmt="action", pager=pager, outfile=outfile)
                 break
 
-
-def ts_send_command(device: CentralObject, cmd: str, outfile: Path, pager: bool,) -> None:
-    """Helper command to send troubleshooting output (user provides command) and print results
-
-    Args:
-        device (CentralObject): Device Object
-        cmd (str): User provided command
-        outfile (Path): Optional output to file
-        pager (bool): Optional Use Pager
-    """
-    console = Console(emoji=False)
-    dev = cli.cache.get_dev_identifier(device)
-    dev_type = lib_to_api("tshoot", dev.type)
-    if len(cmd) == 1:
-        cmd = cmd[0].split()
-    cmd = " ".join(cmd)
-    cmd = cmd.replace("  ", " ").strip().lower()
-    resp = cli.central.request(cli.central.get_ts_commands, dev_type)
-    if not resp:
-        print('[bright_red]Unable to get troubleshooting command list')
-        cli.display_results(resp)
-    else:
-        cmd_list = resp.output
-        cmd_id = [c["command_id"] for c in cmd_list if c["command"].strip() == cmd]
-        if not cmd_id:
-            if FUZZ:
-                fuzz_match, fuzz_confidence = process.extract(cmd, [c["command"].strip() for c in cmd_list], limit=1)[0]
-                print(f"[bright_red]{cmd}[/] is not a valid troubleshooting command (supported by API) for {dev.type}.")
-                confirm_str = render.rich_capture(f"Did you mean [green3]{fuzz_match}[/]?")
-                if fuzz_confidence >= 70 and typer.confirm(confirm_str):
-                    cmd_id = [c["command_id"] for c in cmd_list if c["command"].strip() == fuzz_match]
-
-        if not cmd_id:
-            caption = f'[bright_red]Error[/]: [cyan]{cmd}[/] not found in available troubleshooting commands for {dev.type}. See available commands above.'
-            cli.display_results(resp, tablefmt="rich", caption=caption, title=f"Available troubleshooting commands for {dev.type}", cleaner=cleaner.show_ts_commands)
-        else:
-            resp = cli.central.request(cli.central.start_ts_session, dev.serial, dev_type=dev_type, commands=cmd_id)
-            cli.display_results(resp, tablefmt="action", exit_on_fail=True)
-
-            complete = False
-            while not complete:
-                for x in range(3):
-                    with console.status("Waiting for Troubleshooting Response..."):
-                        sleep(10)
-                    ts_resp = cli.central.request(cli.central.get_ts_output, dev.serial, resp.session_id)
-
-                    if ts_resp.output.get("status", "") == "COMPLETED":
-                        console.print(ts_resp.output["output"])
-                        complete = True
-                        break
-                    else:
-                        print(f'{ts_resp.output.get("message", " . ").split(".")[0]}. [cyan]Waiting...[/]')
-
-
-                if not complete:
-                    print(f'[dark_orange3]WARNING[/] Central is still waiting on response from [cyan]{dev.name}[/]')
-                    if not typer.confirm("Continue to wait/retry?"):
-                        cli.display_results(ts_resp, tablefmt="action", pager=pager, outfile=outfile)
-                        break
-
 @app.command(short_help="Send troubleshooting command to a device")
 def command(
     device: str = typer.Argument(..., metavar=iden_meta.dev, autocompletion=cli.cache.dev_completion, show_default=False,),
@@ -402,6 +455,8 @@ def command(
 
     Returns response (from device) to troubleshooting commands.
     Commands must be supported by the API, use [cyan]show tshoot commands <dev-type>[/] to see available commands
+
+    [bright_green]Example:[/] [cyan]cencli tshoot command barn.518.2816-ap show ap-env[/]
     """
     ts_send_command(device, cmd, outfile, pager)
 
