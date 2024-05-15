6f15e7b0bf8031f674c4ebaf669496660513ab02
wade ~ Pack3tL0ss <wade99 at github>
2024-05-14 19:11:09 -0500

    :white_check_mark: fix test_dev_ap_gw_completion test, build a ctx to work with updated completion method

--- tests/test_completion.py
+++ tests/test_completion.py
@@ -4,9 +4,11 @@ from typing import List, Union
 from cli import app  # type: ignore # NoQA
 from typer.testing import CliRunner
 from centralcli import cache
+from click import Context, Command
 
 runner = CliRunner()
-
+ctx = Context(Command("cencli reset"), info_name="reset", resilient_parsing=True)
+ctx.params={'what': 'overlay', 'device': None, 'yes': None, 'debug': None, 'default': None, 'account': None}
 # TODO make this work
 # work in progress based on https://stackoverflow.com/questions/9137245/unit-test-for-bash-completion-script
 # need to see of typer command runner has a function to test completion
@@ -88,7 +90,7 @@ def test_dev_completion_case_insensitive(incomplete: str = "BSmT"):
     assert all([m.lower().startswith(incomplete.lower()) for m in [c if isinstance(c, str) else c[0] for c in result]])
 
 def test_dev_ap_gw_completion(incomplete: str = "ant"):
-    result = [c for c in cache.dev_ap_gw_completion(incomplete, ["show", "overlay", "summary"])]
+    result = [c for c in cache.dev_ap_gw_completion(ctx=ctx, incomplete=incomplete, args=["show", "overlay", "summary"])]
     assert len(result) > 0
     assert all([m.lower().startswith(incomplete.lower()) for m in [c if isinstance(c, str) else c[0] for c in result]])
 
