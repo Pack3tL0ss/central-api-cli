6916df5a7b68f1bbd07255aca848b6b8b318a03a
user <wade99 at github>
2024-09-29 18:53:55 -0500

    :label: Add SiteData TypeDef

--- centralcli/typedefs.py
+++ centralcli/typedefs.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 from yarl import URL
 
-from typing import Literal
+from typing import Literal, List, Dict
 
 
 
 StrOrURL = str | URL
 Method = Literal['GET', 'POST', 'PUT', 'DELETE']
+SiteData = List[Dict[str, str | int | float]] | Dict[str, str | int | float]
