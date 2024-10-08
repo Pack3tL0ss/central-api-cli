29302b894e4dca8ce329281abcbd80ec1a2f192f
user <wade99 at github>
2024-10-08 15:52:11 -0500

    :white_check_mark: Change assert to match change in output.

--- tests/test_show.py
+++ tests/test_show.py
@@ -245,7 +245,7 @@ def test_show_cx_switch_lldp_neighbors():
     print(result.stdout)
     assert result.exit_code == 0
     assert "chassis" in result.stdout
-    assert "remote port" in result.stdout
+    assert "remote port" in result.stdout.replace("_", " ")
 
 
 def test_show_groups():
