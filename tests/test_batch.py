267c08bc807b676b5aa375acfba744e38e318578
user <wade99 at github>
2024-10-09 11:26:08 -0500

    :white_check_mark: Add batch tests for groups

--- tests/test_batch.py
+++ tests/test_batch.py
@@ -1,12 +1,28 @@
 from typer.testing import CliRunner
 
 from cli import app  # type: ignore # NoQA
-from . import test_site_file, test_data, test_batch_device_file
+from . import test_site_file, test_group_file, test_data, test_batch_device_file
 import traceback
 from rich.traceback import Traceback
 from rich import print
+from centralcli import cache
 
 runner = CliRunner()
+
+def test_batch_add_groups():
+    result = runner.invoke(app, ["batch", "add",  "groups", str(test_group_file), "-Y"])
+    assert result.exit_code == 0
+    assert len([g for g in cache.groups_by_name.keys() if g.startswith("cencli_test_group")]) == len(test_data["batch"]["groups_by_name"])
+
+
+def test_batch_del_groups():
+    result = runner.invoke(app, ["batch", "delete",  "groups", str(test_group_file), "-Y"])
+    if test_group_file.is_file():
+        test_group_file.unlink()
+    assert result.exit_code == 0
+    assert "success" in result.stdout.lower()
+    assert result.stdout.lower().count("success") == len(test_data["batch"]["groups_by_name"])
+
 
 def test_batch_add_sites():
     result = runner.invoke(app, ["batch", "add",  "sites", str(test_site_file), "-Y"])
