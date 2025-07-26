132fb0a8c8d3f257f82883718b5a729d74c161e2
user <wade99 at github>
2025-07-26 10:10:51 -0500

    🎨 Change how GlpApi class is instantiated

--- centralcli/cnx/api/__init__.py
+++ centralcli/cnx/api/__init__.py
@@ -4,14 +4,17 @@ from functools import cached_property
 from typing import TYPE_CHECKING
 from .glp.devices import GlpDevicesApi
 from .glp.subscriptions import GlpSubscriptionsApi
+from ...client import Session
+from ... import config
 
 
 if TYPE_CHECKING:
-    from ...classic.client import Session
+    from ...typedefs import StrOrURL
+    from aiohttp.client import ClientSession
 
 class GlpApi:
-    def __init__(self, session: Session):
-        self._session = session
+    def __init__(self, base_url: StrOrURL = None, aio_session: ClientSession = None, silent: bool = True):
+        self._session = Session(base_url=base_url or config.glp.base_url, aio_session=aio_session, silent=silent, cnx=True)
 
     @property
     def session(self) -> Session:
