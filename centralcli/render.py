5f1bf67c651160f81df2634814dd65c8e92cc701
user <wade99 at github>
2025-08-18 21:19:52 -0500

    ⚰️ Remove code relocated to response.py

--- centralcli/render.py
+++ centralcli/render.py
@@ -1022,18 +1022,6 @@ def display_results(
             tablefmt = "raw"
 
         for idx, r in enumerate(resp):
-            # try:
-            #     if config.dev.capture_raw:  # and r.method == "GET":
-            #         with Spinner("Capturing raw response"):
-            #             raw = r.raw if r.url.path in r.raw else {r.url.path: r.raw}
-            #             raw = {f"{r.method}_{k}": v for k, v in r.raw.items()}  # TODO Do this in the Response object
-            #             if not config.capture_file.exists():
-            #                 config.capture_file.write_text("[")
-            #             with config.capture_file.open("a") as f:
-            #                 f.write(f"{json.dumps(raw)},")
-            # except Exception as e:
-            #     log.error(f"Exception whilte attempting to capture raw output {repr(e)}")
-
             # Multi request url line (example below)
             # Request 1 [POST: /platform/device_inventory/v1/devices]
             #  Response:
