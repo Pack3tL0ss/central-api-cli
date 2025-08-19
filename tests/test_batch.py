8609193c9caa81af9414fd5c96801d740834b6de
user <wade99 at github>
2025-08-18 21:24:04 -0500

    🤡 🚚 Move all delete tests to last module for the sake of cache and mocked response capture

--- tests/test_batch.py
+++ tests/test_batch.py
@@ -1,27 +1,20 @@
+import traceback
+
+from rich import print
+from rich.traceback import Traceback
 from typer.testing import CliRunner
 
 from centralcli.cli import app  # type: ignore # NoQA
-from . import test_site_file, test_group_file, test_data, test_batch_device_file
-import traceback
-from rich.traceback import Traceback
-from rich import print
 
+from . import test_batch_device_file, test_data, test_group_file, test_site_file
 
 runner = CliRunner()
+
 
 def test_batch_add_groups():
     result = runner.invoke(app, ["batch", "add",  "groups", str(test_group_file), "-Y"])
     assert result.exit_code == 0
     assert result.stdout.lower().count("created") == len(test_data["batch"]["groups_by_name"]) + 1  # Plus 1 as confirmation prompt includes "created"
-
-
-def test_batch_del_groups():
-    result = runner.invoke(app, ["batch", "delete",  "groups", str(test_group_file), "-Y"])
-    if test_group_file.is_file():
-        test_group_file.unlink()
-    assert result.exit_code == 0
-    assert "success" in result.stdout.lower()
-    assert result.stdout.lower().count("success") == len(test_data["batch"]["groups_by_name"])
 
 
 def test_batch_add_sites():
@@ -31,14 +24,6 @@ def test_batch_add_sites():
     assert "state" in result.stdout or "_DUPLICATE_SITE_NAME" in result.stdout
 
 
-def test_batch_del_sites():
-    result = runner.invoke(app, ["batch", "delete",  "sites", str(test_site_file), "-Y"])
-    if test_site_file.is_file():
-        test_site_file.unlink()
-    assert result.exit_code == 0
-    assert "success" in result.stdout
-    assert result.stdout.count("success") == len(test_data["batch"]["sites"])
-
 def test_batch_unarchive_devices():
     result = runner.invoke(app, ["batch", "unarchive",  "--yes", f'{str(test_batch_device_file)}'])
     assert result.exit_code == 0
@@ -46,6 +31,7 @@ def test_batch_unarchive_devices():
     if result.exception:
         print(Traceback())
         traceback.print_exception(result.exception)
+
 
 def test_batch_add_devices():
     result = runner.invoke(app, ["batch", "add",  "devices", f'{str(test_batch_device_file)}', "-Y"])
@@ -56,9 +42,3 @@ def test_batch_add_devices():
     if result.exception:
         print(Traceback())
         traceback.print_exception(result.exception)
-
-def test_batch_del_devices():
-    result = runner.invoke(app, ["batch", "delete",  "devices", f'{str(test_batch_device_file)}', "-Y"])
-    assert result.exit_code == 0
-    assert "subscriptions successfully removed" in result.stdout.lower()
-    assert "200" in result.stdout
