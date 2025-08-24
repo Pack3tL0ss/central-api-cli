730b462e2d0bd3677e94599c18e9c51199a8bef1
user <wade99 at github>
2025-08-24 12:48:11 -0500

    ✅ handle string (path) as value in test_data.yaml for batch

--- tests/_test_data.py
+++ tests/_test_data.py
@@ -2,7 +2,7 @@ import json
 from pathlib import Path
 from typing import Any
 
-from centralcli import config
+from centralcli import common, config
 from centralcli.exceptions import ImportException
 
 
@@ -14,15 +14,20 @@ def get_test_data():
 
 
 def setup_batch_import_file(test_data: dict | str, import_type: str = "sites") -> Path:
-    if isinstance(test_data["batch"][import_type], str):
-        return Path(test_data["batch"][import_type])  # pragma: no cover
-
     test_batch_file = config.cache_dir / f"test_runner_{import_type}.json"
+
+    if isinstance(test_data["batch"][import_type], str):  # pragma: no cover
+        seed_file = Path(test_data["batch"][import_type])
+        data = common._get_import_file(seed_file, import_type=import_type)
+    else:
+        data = test_data["batch"][import_type]
+
     res = test_batch_file.write_text(
-        json.dumps(test_data["batch"][import_type])
+        json.dumps(data)
     )
     if not res:
         raise ImportException("Batch import file creation from test_data returned 0 chars written")  # pragma: no cover
+
     return test_batch_file
 
 
