64aa17d1af9ecba74da6a60aa6c8cff8db0f3b18
wade ~ Pack3tL0ss <wade99 at github>
2025-10-13 19:49:23 -0500

    ♻️ env.current_test strip off parametrize from name.

--- centralcli/environment.py
+++ centralcli/environment.py
@@ -10,15 +10,8 @@ env_var = EnvVar()
 classic_env_var = EnvVar("ARUBACLI_ACCOUNT", "ARUBACLI_DEBUG")
 
 
-def _get_current_test() -> str | None:  # pragma: no cover only used when capturing responses for tests
-    cur_test = environ.get("PYTEST_CURRENT_TEST")
-    if cur_test:
-        return cur_test.split("::")[1].split()[0]
-
-
 class Env:
     _is_pytest: bool = bool(environ.get("PYTEST_VERSION"))
-    _cur_test: str | None = _get_current_test
 
     def __init__(self):
         self.workspace: str = environ.get(env_var.workspace) or environ.get(classic_env_var.workspace)
@@ -49,7 +42,9 @@ class Env:
 
     @property
     def current_test(self) -> str | None:  # pragma: no cover only used when capturing responses for tests
-        return self._cur_test
+        cur_test = environ.get("PYTEST_CURRENT_TEST")
+        if cur_test:
+            return cur_test.split("::")[1].split()[0].split("[")[0]
 
     @current_test.setter
     def current_test(self, value: str | None) -> str | None:  # pragma: no cover  This only hits outside of pytest used with --test <name of test> which implies --capture-raw to capture response for use in automated testing.
