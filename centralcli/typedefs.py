66cd53945b933c71fb039328d07409c53f3cc259
wade ~ Pack3tL0ss <wade99 at github>
2025-11-04 19:54:03 -0600

    🏷️ Add Literal for CloudAuthTimeWindow

--- centralcli/typedefs.py
+++ centralcli/typedefs.py
@@ -41,6 +41,7 @@ CloudAuthUploadTypes = Literal["mpsk", "mac"]
 TableFormat = Literal["json", "yaml", "csv", "rich", "tabulate", "simple", "action"]
 LogType = Literal["event", "audit"]
 InsightSeverityType = Literal["high", "med", "low"]
+CloudAuthTimeWindow = Literal["3h", "1d", "1w", "1M", "3M"]
 JSON_TYPE = Union[List, Dict, str]
 
 class CacheSiteDict(TypedDict):
